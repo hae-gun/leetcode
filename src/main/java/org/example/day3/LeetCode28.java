2cd71b34e78aeb8c4fe833cb1452d25520e77744
Hae-gun <newchk610@gmail.com>
2023-12-23 23:20:30 +0900

    feat: LeetCode28

---
+++ src/main/java/org/example/day3/LeetCode28.java
@@ -0,0 +1,10 @@
+package org.example.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode28 {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
---
+++ src/test/java/org/example/day3/LeetCode28Test.java
@@ -0,0 +1,18 @@
+package org.example.day3;
+
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LeetCode28Test {
+    LeetCode28 solution = new LeetCode28();
+
+    @Test
+    void test(){
+        String haystack = "sadbutsad", needle = "sad";
+        int result = solution.strStr(haystack, needle);
+        assertThat(result).isEqualTo(0);
+    }
+}
