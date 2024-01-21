7ddbb1c0dee7bd91d5afe503742d59a446e67d77
Hae-gun <newchk610@gmail.com>
2024-01-21 21:11:10 +0900

    feature: LeetCode56 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,24 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "[[\"8\",\"3\",\".\",\".\",\"7\",\".\",\".\",\".\",\".\"]\n" +
-                ",[\"6\",\".\",\".\",\"1\",\"9\",\"5\",\".\",\".\",\".\"]\n" +
-                ",[\".\",\"9\",\"8\",\".\",\".\",\".\",\".\",\"6\",\".\"]\n" +
-                ",[\"8\",\".\",\".\",\".\",\"6\",\".\",\".\",\".\",\"3\"]\n" +
-                ",[\"4\",\".\",\".\",\"8\",\".\",\"3\",\".\",\".\",\"1\"]\n" +
-                ",[\"7\",\".\",\".\",\".\",\"2\",\".\",\".\",\".\",\"6\"]\n" +
-                ",[\".\",\"6\",\".\",\".\",\".\",\".\",\"2\",\"8\",\".\"]\n" +
-                ",[\".\",\".\",\".\",\"4\",\"1\",\"9\",\".\",\".\",\"5\"]\n" +
-                ",[\".\",\".\",\".\",\".\",\"8\",\".\",\".\",\"7\",\"9\"]]";
+        String s = "[[1,3],[2,6],[8,10],[15,18]]";
 
-        System.out.println(s.replaceAll("\\[", "")
-                .replaceAll("\\]","")
-                .replaceAll("\\\"", "")
-                .replaceAll(",","")
-                .replaceAll("\n", "/"));
+        System.out.println(Arrays.toString(make2DArray(s)));
     }
+
+
+    public static int[][] make2DArray(String arrStr){
+        // [[1,3],[2,6],[8,10],[15,18]]
+        arrStr = arrStr.replaceAll("\\],\\[", ".")
+                        .replaceAll("\\[","")
+                        .replaceAll("\\]","");
+
+        return Arrays.stream(arrStr.split("\\."))
+                .map(v -> Arrays.stream(v.split(",")).mapToInt(Integer::valueOf).toArray())
+                .toArray(int[][]::new);
+    }
+
 }
