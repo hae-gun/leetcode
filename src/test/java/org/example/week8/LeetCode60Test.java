521975fe334f16ed69419313d25766aa395d5cbe
hae-gun <newchk610@gmail.com>
2024-01-28 22:28:07 +0900

    LeetCode60Test.java 업데이트

--- src/test/java/org/example/week8/LeetCode60Test.java
+++ src/test/java/org/example/week8/LeetCode60Test.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LeetCode60Test {
     LeetCode60 solution = new LeetCode60();
+   
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @CsvSource(value = {"3:3:213","4:9:2314", "3:1:123"}, delimiter = ':')
     void test(int n, int k, String result){
