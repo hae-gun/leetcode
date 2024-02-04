d9ca3eab48b43748c43b023f418e163ab8681ac5
Hae-gun <newchk610@gmail.com>
2024-02-04 23:54:40 +0900

    feature: LeetCode68 문제 풀이

--- src/main/java/org/example/week9/LeetCode68.java
+++ src/main/java/org/example/week9/LeetCode68.java
@@ -2,27 +2,83 @@ package org.example.week9;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LeetCode68 {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> list = new ArrayList<>();
-
-        int idx = 0;
-        List<String>
-        for(String word : words){
-            idx += word.length();
-
-            if(idx > maxWidth){
-                list.add(temp.toString());
-                temp = new StringBuilder();
-                idx = 0;
-                continue;
-            }
-
+        List<String> result = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        if (words.length==1){
+            stack.add(words[0]);
+            result.add(lineJustify(stack,maxWidth));
+            return result;
         }
-
-
-
-        return list;
+        int length=0;
+        int index=0;
+        while(index<words.length){
+            if(length==0){
+                stack.clear();
+                stack.add(words[index]);
+                length=words[index].length();
+                index++;
+                continue;
+            }else{
+                if (length+1+words[index].length()<=maxWidth){
+                    stack.add(words[index]);
+                    length+=1+words[index].length();
+                    index++;
+                    continue;
+                }else {
+                    result.add( lineJustify(stack,maxWidth) );
+                    stack.clear();
+                    length=0;
+                    continue;
+                }
+            }
+        }
+        if (!stack.empty()){
+            String flag = "";
+            int blank = maxWidth;
+            for (String word:stack) {
+                blank-=word.length();
+            }
+            blank-=stack.size();
+            for (int i = 0; i < blank; i++) {
+                flag+=" ";
+            }
+            stack.add(flag);
+            result.add(lineJustify(stack,maxWidth));
+        }
+        return result;
     }
+
+    public String lineJustify(Stack<String> stack, int maxWidth){
+        String result = "";
+        if (stack.size()==1){
+            result+=stack.peek();
+            for (int i = 0; i < maxWidth-stack.peek().length(); i++) {
+                result+=" ";
+            }
+        }else {
+            int total=0;
+            for (String word:stack) {
+                total+=word.length();
+            }
+            int rest=(maxWidth-total)%(stack.size()-1),single=(maxWidth-total)/(stack.size()-1);
+            int now;
+            for (String word:stack) {
+                result+=word;
+                if (word==stack.peek()) break;
+                for (int i = 0; i < single; i++) {
+                    result+=" ";
+                }
+                if (rest>0){
+                    result+=" ";
+                    rest--;
+                }
+            }
+        }
+        return result;
+    }
+
 }
--- src/test/java/org/example/week9/LeetCode68Test.java
+++ src/test/java/org/example/week9/LeetCode68Test.java
@@ -10,9 +10,9 @@ public class LeetCode68Test {
 
     LeetCode68 solution = new LeetCode68();
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @CsvSource(value = {"This,is,an,example,of,text,justification.:16:This    is    an,example  of text,justification.",
-    "What,must,be,acknowledgment,shall,be:16:What   must   be,acknowledgment  ,shall be",
-    "Science,is,what,we,understand,well,enough,to,explain,to,a,computer.,Art,is,everything,else,we,do:20:Science  is  what we,understand      well,enough to explain to,a  computer.  Art is,everything  else  we,do                  "},delimiter = ':')
+    @CsvSource(value = {"This,is,an,example,of,text,justification.:16:This    is    an,example  of text,justification.  ,",
+    "What,must,be,acknowledgment,shall,be:16:What   must   be,acknowledgment  ,shall be        ,",
+    "Science,is,what,we,understand,well,enough,to,explain,to,a,computer.,Art,is,everything,else,we,do:20:Science  is  what we,understand      well,enough to explain to,a  computer.  Art is,everything  else  we,do                  ,"},delimiter = ':')
     void test(String words, int maxWidth, String resultStr){
 
         String[] wordsArr = words.split(",");
