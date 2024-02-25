e34f3d640baf81caa626ee66a593dd5347bcd5f6
Hae-gun <newchk610@gmail.com>
2024-02-25 23:02:37 +0900

    feature: LeetCode88 solve

--- src/main/java/org/example/week12/LeetCode88.java
+++ src/main/java/org/example/week12/LeetCode88.java
@@ -7,7 +7,6 @@ public class LeetCode88 {
         int i = m - 1; // nums1의 인덱스
         int j = n - 1; // nums2의 인덱스
         int k = m + n - 1; // 결과를 저장하는 nums1의 인덱스
-
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
