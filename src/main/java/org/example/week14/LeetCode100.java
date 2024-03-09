5b0b087f7cc535b01a21b9ce8c53a2f3838e9d34
Hae-gun <newchk610@gmail.com>
2024-03-09 21:27:43 +0900

    feature : LeetCode100 solved

--- src/main/java/org/example/week14/LeetCode100.java
+++ src/main/java/org/example/week14/LeetCode100.java
@@ -10,8 +10,8 @@ public class LeetCode100 {
         if((p!=null && q == null) || (p==null && q!=null)){
             return false;
         }
-        if(p != null && q != null){
-            if(p.val != q.val) return false;
+        if(p != null && q != null && p.val != q.val){
+            return false;
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
