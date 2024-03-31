258773f57fb267c0d4e54181e48ab7fc99bc389c
Hae-gun <newchk610@gmail.com>
2024-03-31 20:55:59 +0900

    feature: LeetCode113 solved

--- src/main/java/org/example/week17/LeetCode113.java
+++ src/main/java/org/example/week17/LeetCode113.java
@@ -1,9 +1,40 @@
 package org.example.week17;
 
+import org.example.week13.TreeNode;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LeetCode113 {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(root.val);
+        dfs ( root, targetSum - root.val, result, nums);
 
+        return result;
+    }
+
+    private void dfs(TreeNode root, int targetSum, List<List<Integer>> result, ArrayList<Integer> nums) {
+
+        if (root.right == null && root.left == null && targetSum == 0){
+            result.add(nums);
+            return;
+        }
+
+
+
+        if(root.right != null){
+            ArrayList<Integer> otherNums = new ArrayList<>(nums);
+            otherNums.add(root.right.val);
+            dfs(root.right, targetSum - root.right.val, result, otherNums);
+        }
+
+        if(root.left != null){
+            ArrayList<Integer> otherNums = new ArrayList<>(nums);
+            otherNums.add(root.left.val);
+            dfs(root.left, targetSum - root.left.val, result, otherNums);
+        }
     }
 }
