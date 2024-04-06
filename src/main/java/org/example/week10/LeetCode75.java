75765eef74fbfeb726e522014db88d55f047eeeb
Hae-gun <newchk610@gmail.com>
2024-04-06 21:58:08 +0900

    feature: solved LeetCode117

--- src/main/java/org/example/week10/LeetCode75.java
+++
@@ -1,9 +0,0 @@
-package org.example.week10;
-
-import java.util.Arrays;
-
-public class LeetCode75 {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-    }
-}
--- src/main/java/org/example/week3/LeetCode28.java
+++
@@ -1,7 +0,0 @@
-package org.example.week3;
-
-public class LeetCode28 {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-    }
-}
