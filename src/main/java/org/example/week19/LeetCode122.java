69fa6f2f4062f7f6051e42f0f0a8570a4a093de3
Hae-gun <newchk610@gmail.com>
2024-04-11 21:30:07 +0900

    feature: LeetCode122

--- src/main/java/org/example/week19/LeetCode122.java
+++ src/main/java/org/example/week19/LeetCode122.java
@@ -1,2 +1,20 @@
-package org.example.week19;public class LeetCode122 {
+package org.example.week19;
+
+public class LeetCode122 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buyIndex = 0;
+
+        for (int sellIndex = 0; sellIndex < prices.length - 1; sellIndex++) {
+            if (prices[sellIndex + 1] < prices[sellIndex]) {
+                profit += prices[sellIndex] - prices[buyIndex];
+                buyIndex = sellIndex + 1;
+            }
+        }
+        profit += prices[prices.length - 1] - prices[buyIndex];
+
+        return profit;
+
+    }
+
 }
