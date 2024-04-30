5332fd401e121b5c4cbe78866f7b461d9cfce945
Hae-gun <newchk610@gmail.com>
2024-04-30 22:06:54 +0900

    feautre: solved LeetCode139

---
+++ src/main/java/org/example/week22/LeetCode139.java
@@ -0,0 +1,25 @@
+package org.example.week22;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class LeetCode139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> hs = new HashSet<>();
+        for(String str : wordDict) hs.add(str);
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = i - 1; j >= 0; j--){
+                if(dp[j] && hs.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
